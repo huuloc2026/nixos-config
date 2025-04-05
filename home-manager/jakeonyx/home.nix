165f4d378d532e9812bb4ece21ab0ddf93afedb3
Jake Onyx <huuloc2026@gmail.com>
2025-04-05 23:25:25 +0700

    update src

--- home-manager/jakeonyx/home.nix
+++ home-manager/jakeonyx/home.nix
@@ -27,8 +27,8 @@ in
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
+      vscode
+   # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
