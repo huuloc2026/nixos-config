b809716a36b2a619d7b027502f657df1dfd0f5c5
Huu Loc <huuloc2026@gmail.com>
2025-04-05 16:57:38 +0700

    init flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "My Flakes - Jake Onyx";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11"; 
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+
+  outputs = { self, nixpkgs, home-manager,... }:
+  let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs= nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+    homeConfigurations = {
+        jakeonyx = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+          ./home.nix
+        ];
+      };
+    };
+  };
+}
+
