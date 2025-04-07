05723af7fa0beefeb0cec7d842489d519ba4ea9b
JakeOnyx <huuloc2026@gmail.com>
2025-04-07 18:10:11 +0700

    update

--- home-manager/modules/extension/extensionconfig.nix
+++ home-manager/modules/extension/extensionconfig.nix
@@ -4,6 +4,7 @@
   dconf.settings = {
     "org/gnome/shell" = {
       enabled-extensions = [
+        "system-monitor@gnome-shell-extensions.gcampax.github.com"
         "dash-to-dock@micxgx.gmail.com"
         "clipboard-indicator@tudmotu.com"
       ];
@@ -12,7 +13,7 @@
     "org/gnome/shell/extensions/dash-to-dock" = {
       dock-position = "RIGHT";
       extend-height = false;
-      intellihide=true;
+      intellihide = true;
       autohide = true;
       use-built-in-theme = true;        
       show-trash = false;     
